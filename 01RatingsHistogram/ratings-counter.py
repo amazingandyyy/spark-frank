d6c4d1d9d5d2a69efa36485deda570309320fc9c
Amazingandyyy <amazingandyyy@gmail.com>
2017-04-07 18:28:48 -0700

    add 02

--- 01RatingsHistogram/ratings-counter.py
+++ 01RatingsHistogram/ratings-counter.py
@@ -1,13 +1,14 @@
 from pyspark import SparkConf, SparkContext
-import collections
+import collections # python built-in pkgs
 
 conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
 sc = SparkContext(conf = conf)
 
-lines = sc.textFile("./ml-100k/u.data")
+lines = sc.textFile("./ml-100k/u.data") # every line will be RDD
 ratings = lines.map(lambda x: x.split()[2])
 result = ratings.countByValue()
-
+# print("result", sorted(result.items()))
 sortedResults = collections.OrderedDict(sorted(result.items()))
+# print("sortedResults", sortedResults.items())
 for key, value in sortedResults.items():
     print("%s %i" % (key, value))
---
+++ datasetResources.md
@@ -0,0 +1,3 @@
+# Dataset used in this course
+
+01 Movie rating data: [grouplens](https://grouplens.org/datasets/movielens/)
